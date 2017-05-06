52e68fb97a3a15c1c8e9debb29a978f4c93a48dc
Changbeom Choi <cbchoi@handong.edu>
2017-05-06 09:07:30 +0900

    Tinybmp file update

--- Homework/TinyBitmapMaker/tinybmp_maker/tinybmp.cpp
+++ Homework/TinyBitmapMaker/tinybmp_maker/tinybmp.cpp
@@ -11,6 +11,11 @@ TinyBitmap::TinyBitmap(const char* _path)
 	std::ifstream fin;
 	fin.open(_path, std::ifstream::binary);
 	fin.read((char*)&m_header, sizeof(m_header));
+	fin.read((char*)&m_info_header, sizeof(m_info_header));
 
+	// File Type Checking
+
+    //
 	fin.close();
 }
+
